417076d1eba7bc84fb02298077f8ea995bf18455
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-03-01 16:18:42 +0300

    refactor: UserInputException.java

--- src/main/java/home_work_1/producer_queue_consumer/exceptions/UserInputException.java
+++ src/main/java/home_work_1/producer_queue_consumer/exceptions/UserInputException.java
@@ -1,6 +1,10 @@
 package home_work_1.producer_queue_consumer.exceptions;
 
 public class UserInputException extends Exception {
+    public UserInputException() {
+        super("User input Exception");
+    }
+
     public UserInputException(String message) {
         super(message);
     }
