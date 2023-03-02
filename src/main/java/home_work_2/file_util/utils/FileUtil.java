8f1c9b3d5d17a8a3b1296d3c0ee553ca7aee1441
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-03-03 00:43:36 +0300

    feat: FileUtil.listOfVowelWords

--- src/main/java/home_work_2/file_util/utils/FileUtil.java
+++ src/main/java/home_work_2/file_util/utils/FileUtil.java
@@ -2,9 +2,10 @@ package home_work_2.file_util.utils;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-/** Класс утилита
+/** Класс утилита для работы с текстовыми файлами
  @author Dzmitry Krasiuk
  */
 public class FileUtil {
@@ -21,8 +22,6 @@ public class FileUtil {
             while ((symbol = reader.readLine()) != null) {
                 result.append(symbol.toUpperCase()).append("\n");
             }
-
-            System.out.println(result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -34,9 +33,9 @@ public class FileUtil {
         }
     }
 
-    /** Возвращает список строк для исходного файла
+    /** Возвращает список строк для файла
      * @param source путь к исходному файлу
-     * @return список строк файла
+     * @return список строк
      */
     public List<String> listOfStrings(String source) {
         List<String> stringList = new ArrayList<>();
@@ -47,10 +46,45 @@ public class FileUtil {
             while ((symbol = reader.readLine()) != null) {
                 stringList.add(symbol);
             }
-
-            System.out.println(stringList);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        return stringList;
+    }
+
+    /** Возвращает список слов начинающихся с гласной буквы
+     * @param source путь к исходному файлу
+     * @return список строк
+     */
+    public List<String> listOfVowelWords(String source) {
+        List<String> stringList = new ArrayList<>();
+        StringBuilder tmp = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String symbol;
+
+            while ((symbol = reader.readLine()) != null) {
+                tmp.append(symbol).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] tmpSubStrings = tmp.toString().split("\\s");
+
+        for (String word : tmpSubStrings) {
+            if (word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i'
+                    || word.charAt(0) == 'o' || word.charAt(0) == 'u' || word.charAt(0) == 'y' || word.charAt(0) == 'A'
+                    || word.charAt(0) == 'E' || word.charAt(0) == 'I' || word.charAt(0) == 'O' || word.charAt(0) == 'U'
+                    || word.charAt(0) == 'Y' || word.charAt(0) == 'а' || word.charAt(0) == 'у' || word.charAt(0) == 'о'
+                    || word.charAt(0) == 'ы' || word.charAt(0) == 'э' || word.charAt(0) == 'я' || word.charAt(0) == 'ю'
+                    || word.charAt(0) == 'ё' || word.charAt(0) == 'и' || word.charAt(0) == 'е' || word.charAt(0) == 'А'
+                    || word.charAt(0) == 'У' || word.charAt(0) == 'О' || word.charAt(0) == 'Ы' || word.charAt(0) == 'Э'
+                    || word.charAt(0) == 'Я' || word.charAt(0) == 'Ю' || word.charAt(0) == 'Ё' || word.charAt(0) == 'И'
+                    || word.charAt(0) == 'Е') {
+                stringList.add(word);
+            }
         }
 
         return stringList;
