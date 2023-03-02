42b8855bceeeb7afa5a690e680d1add817530c31
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-03-02 23:12:03 +0300

    refactor: FileUtil.copyContentInUpperReg, FileUtilDemo

--- src/main/java/home_work_2/file_util/utils/FileUtil.java
+++ src/main/java/home_work_2/file_util/utils/FileUtil.java
@@ -1,7 +1,27 @@
 package home_work_2.file_util.utils;
 
+import java.io.*;
+
 public class FileUtil {
     public void copyContentInUpperReg(String source, String destination) {
+        StringBuilder result = new StringBuilder();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String symbol;
+
+            while ((symbol = reader.readLine()) != null) {
+                result.append(symbol.toUpperCase());
+            }
+
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination))) {
+            writer.write(result.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/home_work_2/FileUtilDemo.java
@@ -0,0 +1,9 @@
+package home_work_2;
+
+import home_work_2.file_util.utils.FileUtil;
+
+public class FileUtilDemo {
+    public static void main(String[] args) {
+        FileUtil fileUtil = new FileUtil();
+    }
+}
