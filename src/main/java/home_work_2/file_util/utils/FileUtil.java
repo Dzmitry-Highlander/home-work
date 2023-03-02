3506913349535dfb019d19a0ec8d426fb23531cb
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-03-02 23:27:35 +0300

    refactor: FileUtil.java

--- src/main/java/home_work_2/file_util/utils/FileUtil.java
+++ src/main/java/home_work_2/file_util/utils/FileUtil.java
@@ -2,7 +2,15 @@ package home_work_2.file_util.utils;
 
 import java.io.*;
 
+/** Класс утилита FileUtil
+ @author Dzmitry Krasiuk
+ */
 public class FileUtil {
+    /** Метод, который принимает пути к двум файлам. Метод переписывает содержимое одного файла в другой файл, но
+     * только в ВЕРХНЕМ РЕГИСТРЕ
+     * @param source путь к исходному файлу
+     * @param destination путь к результирующему файлу
+     */
     public void copyContentInUpperReg(String source, String destination) {
         StringBuilder result = new StringBuilder();
 
@@ -23,5 +31,9 @@ public class FileUtil {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void listOfStrings(String source) {
+
     }
 }
