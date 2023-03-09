25edbbd7fd75b0932e02855de0d42424465f9f66
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-03-09 11:44:07 +0300

    feat: TVUtil.java

--- src/main/java/home_work_3/TV/utils/TVUtil.java
+++ src/main/java/home_work_3/TV/utils/TVUtil.java
@@ -1,5 +1,63 @@
 package home_work_3.TV.utils;
 
-public class TVUtil {
+import home_work_3.TV.dao.TV;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class TVUtil {
+    public static List<TV> getTVByDiagonal(List<TV> list, double diagonal) {
+        return list.stream()
+                .filter(n -> n.getDiagonal() == diagonal)
+                .collect(Collectors.toList());
+    }
+
+    public static List<TV> getTVByBrand(List<TV> list, String brand) {
+        return list.stream()
+                .filter(n -> Objects.equals(n.getBrand(), brand))
+                .collect(Collectors.toList());
+    }
+
+    public static List<TV> getTVOlderThanYear(List<TV> list, int year) {
+        return list.stream()
+                .filter(n -> n.getModelYear() > year)
+                .collect(Collectors.toList());
+    }
+
+    public static List<TV> getTVByPriceRange(List<TV> list, double minPrice, double maxPrice) {
+        return list.stream()
+                .filter(n -> (n.getPrice() >= minPrice && n.getPrice() <= maxPrice))
+                .collect(Collectors.toList());
+    }
+
+    public static List<TV> getTVByPrice(List<TV> list, boolean priceAscending) {
+        List<TV> result;
+        if (priceAscending) {
+            result = list.stream()
+                    .sorted(Comparator.comparing(TV::getPrice))
+                    .collect(Collectors.toList());
+        } else {
+            result = list.stream()
+                    .sorted(Comparator.comparing(TV::getPrice).reversed())
+                    .collect(Collectors.toList());
+
+        }
+        return result;
+    }
+
+    public List<TV> getTVByDiagonal(List<TV> list, boolean diagonalAscending) {
+        List<TV> result;
+        if (diagonalAscending) {
+            result = list.stream()
+                    .sorted(Comparator.comparing(TV::getDiagonal))
+                    .collect(Collectors.toList());
+        } else {
+            result = list.stream()
+                    .sorted(Comparator.comparing(TV::getDiagonal).reversed())
+                    .collect(Collectors.toList());
+        }
+        return result;
+    }
 }
